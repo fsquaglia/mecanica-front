a98c5c6e415ed741fa47dd1f561ace5ee4a64e05
Kevinpg123 <osmarkevinp@gmail.com>
2024-03-10 23:12:06 -0300

    avance en la navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,25 +1,31 @@
 import style from './Navbar.module.css'
-import {Link} from 'react-router-dom'
-import {useAuth} from '../Auth/AuthContext/AuthContext'
+import { Link } from 'react-router-dom'
+import { useAuth } from '../Auth/AuthContext/AuthContext'
 
 const Navbar = () => {
-  const {authenticated, user, logout}=useAuth()
-  const handleClick = ()=>{
+  const { authenticated, user, logout } = useAuth()
+  const handleClick = () => {
     logout();
   }
   return (
     <div className={style.nav}>
-        Navbar
-    <Link to='/login'>Ingresar</Link>
-    {authenticated?<>
-     <div className={style.userDetails}>
-        <h4>Bienvenido: {user.nickname&& user.nickname}</h4>
-        <img src={user.picture} alt="Nor Found" />
+      <div className={style.login}>
+        <Link className={style.botones} to='/login'>Ingresar</Link>
+
       </div>
-     </> : null
-    }
-    <a href='/' onClick={handleClick}>Salir</a>
-    <br/>
+
+
+      {
+        authenticated ? <>
+          <div className={style.userDetails}>
+            <h4>Bienvenido: {user.nickname && user.nickname}</h4>
+            <img src={user.picture} alt="Not Found" />
+            <a className={style.out} href='/' onClick={handleClick}>Salir</a>
+          </div>
+        </> : null
+      }
+
+
     </div>
   )
 }
