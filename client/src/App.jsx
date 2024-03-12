c6f2ab45d37043ae55758ee601e440747955b98c
Kevinpg123 <osmarkevinp@gmail.com>
2024-03-12 09:52:53 -0300

    termine la navbar con un menu desplegable

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,38 @@
-import {Routes, Route, Navigate, useNavigate} from 'react-router-dom'
-import {About,  Home, Landing, Login, } from './views/Index'
-import {loginUser, isNotAuth} from './redux/actions'
+import { Routes, Route, Navigate, useNavigate } from 'react-router-dom'
+import { About, Home, Landing, Login, } from './views/Index'
+import { loginUser, isNotAuth } from './redux/actions'
 import { useDispatch } from 'react-redux'
 import { useEffect } from 'react'
 import { useAuth } from './components/Auth/AuthContext/AuthContext'
 import './App.css'
 
 function App() {
-  const {authenticated, user,logout}=useAuth();
+  const { authenticated, user, logout } = useAuth();
   const dispatch = useDispatch();
   console.log(authenticated)
-  const allow = user? user.role : 1;
+  const allow = user ? user.role : 1;
   //console.log(allow)
   const navigate = useNavigate()
-  useEffect(()=>{
-    if(authenticated){
+  useEffect(() => {
+    if (authenticated) {
       dispatch(loginUser(user))
-    }else{
+    } else {
       dispatch(isNotAuth())
     }
-  },[authenticated]);
- 
+  }, [authenticated]);
+
 
   return (
-   <div>
-  <Routes>
-    <Route path= '/' element={<Landing/>}/>
-    <Route path="/home" element= {(authenticated) ? <Home/>: <Navigate to = '/'/>} />
-    {/* <Route path= {'/home/:id'} element={<Detail/>}/> */}
-    <Route path= '/about' element={<About/>}/>
-    <Route path= '/login' element={<Login/>}/>
-    {/* <Route path= {'*'} element={<Error/>}/> */}
-  </Routes>
-   </div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path="/home" element={(authenticated) ? <Home /> : <Navigate to='/' />} />
+        {/* <Route path= {'/home/:id'} element={<Detail/>}/> */}
+        <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
+        {/* <Route path= {'*'} element={<Error/>}/> */}
+      </Routes>
+    </div>
   )
 }
 
