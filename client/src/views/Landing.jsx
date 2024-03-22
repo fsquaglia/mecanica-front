97f601253a828a5f76cb5c2c9188fd1b9cae9926
antorrg <114789901+antorrg@users.noreply.github.com>
2024-03-22 20:50:12 -0300

    Implementacion de react-scroll y componentes landing

--- client/src/views/Landing.jsx
+++ client/src/views/Landing.jsx
@@ -2,13 +2,28 @@ import Carousel from "../components/carousel/Carousel";
 import Navbar from "../components/navbar/Navbar";
 import mocks from "../../public/mocks";
 import { useEffect } from "react";
+import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy } from 'react-scroll';
 import style from './styles/Landing.module.css'
+import { ButtonBack, Consejos, Contacto, Footer, Historia, Pagina, Servicios } from './FrontPage/Index'
 
 const Landing = () => {
-  useEffect(() => {}, []);
+  useEffect(() => {
+     // Actualiza scrollSpy cuando el componente se monta
+     scrollSpy.update();
+
+  }, []);
+
+
   return (
     <div className={style.bigDiv}>
-
+    <Navbar/>
+    <Element name='pagina'><Pagina/></Element>
+    <Element name='historia'><Historia/></Element>
+    <Element name='servicios'><Servicios/></Element>
+    <Element name='consejos'><Consejos/></Element>
+    <Element name='contacto'><Contacto/></Element>
+    <Footer/>
+    <ButtonBack/>
 
       {/* <Carousel info={mocks}/> */}
     </div>
