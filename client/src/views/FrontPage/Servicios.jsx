70255daa7b965e43675da06f8418f04446168612
fsquaglia <fernandosquaglia@gmail.com>
2024-03-24 13:13:04 -0300

    viendo carrusel new

--- client/src/views/FrontPage/Servicios.jsx
+++ client/src/views/FrontPage/Servicios.jsx
@@ -1,29 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { realtimeDB } from "../../firebase/firebaseConfig";
+import { ref, onValue } from "firebase/database";
 
 const Servicios = () => {
+  const [servicesData, setServicesData] = useState(null);
+
+  useEffect(() => {
+    // Referencia al nodo 'services' en la base de datos
+    const servicesRef = ref(realtimeDB, "services");
+
+    // Suscribe un observador para escuchar cambios en 'services'
+    const unsubscribe = onValue(servicesRef, (snapshot) => {
+      const data = snapshot.val();
+      setServicesData(data);
+    });
+
+    // Función de limpieza al desmontar el componente
+    return () => {
+      unsubscribe(); // Desconecta el observador cuando el componente se desmonta
+    };
+  }, []); // Ejecuta el efecto solo una vez al montar el componente
+
   return (
     <div>
-        <h2>Servicios</h2>
-        <p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean feugiat venenatis enim, in sollicitudin augue vehicula eget. Nam blandit pretium tortor vel maximus. Fusce quis pharetra ex, non pellentesque elit. In vitae imperdiet urna, ut hendrerit odio. Vivamus nec tristique nulla. Integer consequat vulputate nibh, et mollis erat feugiat feugiat. Sed scelerisque ex tincidunt mi blandit, eu iaculis risus interdum. Fusce eget neque sit amet mi ornare venenatis. Donec tincidunt rutrum eros, nec vestibulum urna volutpat volutpat. In vel commodo arcu. Sed non euismod mi. Cras tortor lectus, interdum et est non, varius dapibus sem.
-
-Donec commodo mauris vitae est pharetra porttitor. Aliquam ac massa a ipsum gravida feugiat. Nullam quis nisi dui. Nunc sed elementum nisi, quis vehicula massa. Praesent eget semper quam. Etiam viverra aliquet dolor quis vulputate. Aenean condimentum pellentesque dolor vitae blandit.
-
-Nullam finibus neque in sem dignissim, at vehicula tellus efficitur. Fusce aliquet pretium dolor eget volutpat. Morbi scelerisque laoreet diam, at rutrum massa sodales ac. Duis laoreet risus et est pellentesque, in pulvinar leo auctor. Sed in quam laoreet, malesuada orci sed, pharetra nulla. In mollis hendrerit faucibus. Quisque viverra egestas mauris sit amet tincidunt. Aliquam nec mi et sem semper egestas ac eu ipsum. Integer et lectus vitae lorem rutrum viverra ut id erat. Mauris elementum massa at pulvinar molestie. Sed ullamcorper mi a lobortis bibendum.
-
-Aliquam orci mauris, placerat sit amet mauris finibus, tempor efficitur lectus. Integer dictum facilisis sagittis. Nulla tristique nunc enim, tincidunt interdum mi vehicula id. Nunc dui erat, fermentum non massa in, tincidunt dapibus lectus. Integer interdum eget odio sit amet commodo. Cras eleifend orci et erat facilisis, quis molestie sapien sodales. Suspendisse venenatis viverra lorem, eget dapibus tellus ullamcorper in. In bibendum eu nisi et iaculis. Curabitur rhoncus purus at ultrices sollicitudin. Integer eu commodo nulla. Curabitur faucibus vulputate mauris et consectetur. Nam id orci velit. Cras id nibh nisl. Proin dapibus justo vel ipsum scelerisque dapibus. Morbi dignissim hendrerit lacus, accumsan porta arcu vestibulum vitae. Quisque vel magna efficitur, sodales tellus id, gravida massa.
-
-Quisque ut molestie nisl, in interdum justo. Donec et viverra nisi, id tempor nisl. Pellentesque laoreet fringilla eros, et tempor ante. Nam ullamcorper metus eget mattis ultrices. Aenean vitae leo vitae ipsum aliquet porttitor eget quis urna. Morbi ac quam velit. Morbi nec orci mauris. Proin in ullamcorper justo.
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean feugiat venenatis enim, in sollicitudin augue vehicula eget. Nam blandit pretium tortor vel maximus. Fusce quis pharetra ex, non pellentesque elit. In vitae imperdiet urna, ut hendrerit odio. Vivamus nec tristique nulla. Integer consequat vulputate nibh, et mollis erat feugiat feugiat. Sed scelerisque ex tincidunt mi blandit, eu iaculis risus interdum. Fusce eget neque sit amet mi ornare venenatis. Donec tincidunt rutrum eros, nec vestibulum urna volutpat volutpat. In vel commodo arcu. Sed non euismod mi. Cras tortor lectus, interdum et est non, varius dapibus sem.
-
-Donec commodo mauris vitae est pharetra porttitor. Aliquam ac massa a ipsum gravida feugiat. Nullam quis nisi dui. Nunc sed elementum nisi, quis vehicula massa. Praesent eget semper quam. Etiam viverra aliquet dolor quis vulputate. Aenean condimentum pellentesque dolor vitae blandit.
-
-Nullam finibus neque in sem dignissim, at vehicula tellus efficitur. Fusce aliquet pretium dolor eget volutpat. Morbi scelerisque laoreet diam, at rutrum massa sodales ac. Duis laoreet risus et est pellentesque, in pulvinar leo auctor. Sed in quam laoreet, malesuada orci sed, pharetra nulla. In mollis hendrerit faucibus. Quisque viverra egestas mauris sit amet tincidunt. Aliquam nec mi et sem semper egestas ac eu ipsum. Integer et lectus vitae lorem rutrum viverra ut id erat. Mauris elementum massa at pulvinar molestie. Sed ullamcorper mi a lobortis bibendum.
-
-</p>
-<hr></hr>
+      <h2>Servicios</h2>
+      {servicesData ? (
+        <ul>
+          {Object.keys(servicesData).map((serviceKey) => (
+            <li key={serviceKey}>
+              <h3>{servicesData[serviceKey].title}</h3>
+              <p>{servicesData[serviceKey].description}</p>
+              <p>url img: {servicesData[serviceKey].url}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Cargando datos...</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Servicios
+export default Servicios;
