946818f31fd26d99febc4e8792c88e1d49e8ed42
Kevin Osmar Pauluk <osmarkevinp@gmail.com>
2024-04-01 23:48:28 -0300

    responsive del navbar

--- client/src/components/navbar/Links/LoginLinks.jsx
+++ client/src/components/navbar/Links/LoginLinks.jsx
@@ -12,7 +12,7 @@ const LoginLinks = () => {
           <Link to="/login">{user.role === 0 || user.role === 2 ? <a className={style.out} aria-current="page" href="#">Crear usuario</a> : <a className={style.out} aria-current="page" href="#">Ingresar</a>}</Link>
         )
       ) : (
-        <Link to="/login"><a className={style.in} aria-current="page" href="#">Ingresar</a></Link>
+        <Link to="/login"><a className={style.ingresar} aria-current="page" href="#">Ingresar</a></Link>
       )}
       <br></br>
       {authenticated ?
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -88,41 +88,85 @@ const Navbar = () => {
         <span class={style.togglerIcon}>&#9776;</span>
       </button>
       <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel" backdrop={false}>
-        <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">
-            {
-              authenticated ? (`Bienvenido: ${user.nickname}`) : ("Bienvenido")}
-          </h5>
+        <div class={`offcanvas-header ${style.divHeader}`}>
+
           <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          <h2 class={`offcanvas-title ${style.titleDeployMenu}`} id="offcanvasDarkNavbarLabel"> Menú de Navegación</h2>
         </div>
         <div class="offcanvas-body">
+          <br></br>
+
           <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-            <br></br>
-            <li class="nav-item">
+
+
+
+
+            {authenticated ? null : (
+
+              <>
+                <li class="nav-item">
+                  <Link to="pagina" data-bs-dismiss="offcanvas" activeClass="active" spy={true} smooth={true} duration={600} offset={-70} activeStyle={{ color: 'red' }}><h3 className={style.linksH3}>Home</h3> </Link>
+                </li>
+                <li class="nav-item">
+                  <Link to="historia" data-bs-dismiss="offcanvas" activeClass="active" spy={true} smooth={true} duration={600} offset={-70} activeStyle={{ color: 'red' }}> <h3 className={style.linksH3}>Historia</h3> </Link>
+                </li>
+                <li class="nav-item">
+                  <Link to="servicios" data-bs-dismiss="offcanvas" activeClass="active" spy={true} smooth={true} duration={600} offset={-70} activeStyle={{ color: 'red' }}>   <h3 className={style.linksH3}>Servicios</h3> </Link>
+                </li>
+                <li class="nav-item">
+                  <Link to="consejos" data-bs-dismiss="offcanvas" activeClass="active" spy={true} smooth={true} duration={600} offset={-70} activeStyle={{ color: 'red' }}>   <h3 className={style.linksH3}>Tips</h3> </Link>
+                </li>
+                <li class="nav-item">
+                  <Link to="contacto" data-bs-dismiss="offcanvas" activeClass="active" spy={true} smooth={true} duration={600} offset={-70} >  <h3 className={style.linksH3}>Contacto</h3> </Link>
+                </li>
+                <hr></hr>
+              </>
+            )
+            }
+
+
+
+
+
+
+            <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">
+              {
+                authenticated ? (`Bienvenido ${user.nickname}`) : null}
+            </h5>
+            <li class={`nav-item ${style.divPic}`}>
               {
                 authenticated &&
-                <img src={user.picture} alt="Not Found" />
+                <img className={style.pictureDeployMenu} src={user.picture} alt="Not Found" />
               }
             </li>
-            <br></br>
-            <br></br>
-            <li class="nav-item">
-              <NavLink to='/login'>
-                <a className={style.out} aria-current="page" href="#">Ingresar</a>
-              </NavLink>
-            </li>
-            <li class="nav-item">
+
+            {authenticated ? (
+              <>
+                <br></br>
+                <br></br>
+              </>
+            ) : (
+              <>
+                <li class="nav-item" data-bs-dismiss="offcanvas">
+                  <NavLink to='/login'>
+                    <a className={style.out} aria-current="page" href="#">Ingresar</a>
+                  </NavLink>
+                </li>
+              </>
+            )}
+
+            <li class="nav-item" data-bs-dismiss="offcanvas">
               <LoginLinks />
 
 
             </li>
-            <li class="nav-item">
+            <li class="nav-item" data-bs-dismiss="offcanvas">
               <AdminLink />
 
             </li>
 
             <br></br>
-            <li class="nav-item">
+            <li class="nav-item" data-bs-dismiss="offcanvas">
               {
                 authenticated &&
                 <a className={style.out} href='/' onClick={handleClick}>Salir</a>
