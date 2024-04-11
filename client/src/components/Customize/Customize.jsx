838ee82a0c4142a5d0f356fdf8615e0cf8f3f0ee
fsquaglia <fernandosquaglia@gmail.com>
2024-04-11 15:56:24 -0300

    Hasta acá funcionando

--- client/src/components/Customize/Customize.jsx
+++ client/src/components/Customize/Customize.jsx
@@ -1,32 +1,9 @@
 import React, { useState } from "react";
-import { imagesDB } from "../../firebase/firebaseConfig";
-import GenericButton from "../GenericButton/GenericButton";
-import { ref, uploadBytes } from "firebase/storage";
 import ImageLanding from "./ImageLanding ";
 import ImageCarrusel from "./ImageCarrusel";
-
-// Componente para definir el estilo común de los elementos <div>
-function CustomDiv({ className, children, onClick }) {
-  const [isHovered, setIsHovered] = useState(false);
-
-  return (
-    <div
-      className={`col text-light py-3 rounded custom-col ${className}`}
-      style={{
-        cursor: "pointer",
-        boxShadow: isHovered
-          ? "0 0 10px rgba(0, 0, 0, 0.5)"
-          : "6px 6px 10px rgba(0, 0, 0, 0.5)",
-        transition: "box-shadow 0.3s ease",
-      }}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      onClick={onClick}
-    >
-      {children}
-    </div>
-  );
-}
+import CommerceEdit from "./CommerceEdit";
+import DefaultText from "./DefaultText";
+import DivButtonCustom from "./DivButtonCustom";
 
 function Customize() {
   const [showComponent, setShowComponent] = useState({
@@ -37,6 +14,17 @@ function Customize() {
     tips: false,
     comercio: false,
   });
+
+  // Array de componentes que pueden mostrarse
+  const components = [
+    showComponent.principal && <ImageLanding />,
+    showComponent.carrusel && <ImageCarrusel />,
+    showComponent.comercio && <CommerceEdit />,
+    // Agregar más componentes aquí según sea necesario
+  ];
+
+  // Comprueba si al menos uno de los componentes está presente para mostrar el DefaultText
+  const shouldShowDefaultText = !components.some((component) => component);
 
   const handleClick = (nameComponent) => {
     setShowComponent((prevShowComponent) => {
@@ -53,31 +41,48 @@ function Customize() {
       <div style={{ height: "200px" }} className="py-3"></div>
       <h3>Personaliza tu app</h3>
 
-      <div className="row gap-3 py-3">
-        <CustomDiv
+      <div className="row gap-3 py-3 m-2">
+        <DivButtonCustom
           className="bg-success bg-gradient"
           onClick={() => handleClick("principal")}
         >
           Principal
-        </CustomDiv>
-        <CustomDiv
+        </DivButtonCustom>
+        <DivButtonCustom
           className="bg-primary bg-gradient"
           onClick={() => handleClick("carrusel")}
         >
           Carrusel
-        </CustomDiv>
-        <CustomDiv className="bg-danger bg-gradient">Historia</CustomDiv>
-        <CustomDiv className="bg-warning bg-gradient">Servicios</CustomDiv>
-        <CustomDiv className="bg-secondary bg-gradient">Tips</CustomDiv>
-        <CustomDiv className="bg-info bg-gradient">Comercio</CustomDiv>
+        </DivButtonCustom>
+        <DivButtonCustom className="bg-danger bg-gradient">
+          Historia
+        </DivButtonCustom>
+        <DivButtonCustom className="bg-warning bg-gradient">
+          Servicios
+        </DivButtonCustom>
+        <DivButtonCustom className="bg-secondary bg-gradient">
+          Tips
+        </DivButtonCustom>
+        <DivButtonCustom
+          className="bg-info bg-gradient"
+          onClick={() => handleClick("comercio")}
+        >
+          Comercio
+        </DivButtonCustom>
       </div>
-      <div className="container border p-3 radius">
+      <div
+        className="container border border-2 p-3 my-3 rounded"
+        style={{ minHeight: "200px" }}
+      >
         {showComponent.principal
           ? showComponent.principal && <ImageLanding />
           : null}
         {showComponent.carrusel
           ? showComponent.carrusel && <ImageCarrusel />
           : null}
+        {showComponent.comercio && <CommerceEdit />}
+        {/* Mostrar el componente DefaultText si ninguno de los componentes anteriores está visible */}
+        {shouldShowDefaultText && <DefaultText />}
       </div>
     </div>
   );
---
+++ client/src/components/Customize/DefaultText.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DefaultText() {
+  return (
+    <>
+      <p>Selecciona una sección para editar</p>
+    </>
+  );
+}
+
+export default DefaultText;
