1879363d0ccc3ba88723b0584c102d6d00d8f41c
antorrg <114789901+antorrg@users.noreply.github.com>
2024-04-16 09:03:31 -0300

    rutas customize providers y services

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,13 @@
 import interceptor from "./Interceptor";
 import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
-import { Error, Home, Detail, Landing, Login, EditWindow } from "./views/Index";
-import { Admin, DetailAd, DetailService } from "./views/Staff/AdminIndex";
+import { Error, Home, Detail, Landing, Login, EditWindow, Footer, Tips, Agencia } from "./views/Index";
+import { Admin, DetailAd, DetailService, Proveedores, Services, Customize } from "./views/Staff/AdminIndex";
 import { loginUser, isNotAuth, isMyCommerce } from "./redux/actions";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { useAuth } from "./components/Auth/AuthContext/AuthContext";
-import "./App.css";
-import Customize from "./components/Customize/Customize";
 import Navbar from "./components/navbar/Navbar";
-import Footer from "./views/FrontPage/Footer";
-import Tips from "./views/Tips/Tips";
-import Agencia from "./views/FrontPage/Agencia";
+import "./App.css";
 
 function App() {
   const { authenticated, user, logout } = useAuth();
@@ -38,64 +34,23 @@ function App() {
     <div>
       <Navbar />
       <Routes>
-        <Route path="/customize" element={<Customize />} />
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path={"/error"} element={<Error />} />
-        <Route
-          path="/home"
-          element={authenticated ? <Home /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/home/detail/:id"
-          element={authenticated ? <Detail /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/home/detailservice/:id"
-          element={authenticated ? <DetailService /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/home/user/:id"
-          element={authenticated ? <DetailAd /> : <Navigate to="/home" />}
-        />
-        <Route
-          path="/admin/detailservice/:id"
-          element={authenticated ? <DetailService /> : <Navigate to="/home" />}
-        />
-        <Route
-          exact
-          path="/admin"
-          element={
-            (authenticated && allow === 0) || (authenticated && allow === 2) ? (
-              <Admin />
-            ) : (
-              <Navigate to="/home" />
-            )
-          }
-        />
-        <Route
-          path="/admin/:name"
-          element={
-            (authenticated && allow === 0) || (authenticated && allow === 2) ? (
-              <Admin />
-            ) : (
-              <Navigate to="/error" />
-            )
-          }
-        />
-        <Route
-          path="/admin/dett/:id"
-          element={
-            (authenticated && allow === 0) || (authenticated && allow === 2) ? (
-              <DetailAd />
-            ) : (
-              <Navigate to="/error" />
-            )
-          }
-        />
-        <Route path={"*"} element={<Navigate to="/error" />} />
-        <Route path="tips" element={<Tips />} />
-      </Routes>
+        <Route path="/home" element={authenticated ? <Home /> : <Navigate to="/" />} />
+        <Route path="/home/detail/:id" element={authenticated ? <Detail /> : <Navigate to="/" />} />
+        <Route path="/home/detailservice/:id" element={authenticated ? <DetailService /> : <Navigate to="/" />} />
+        <Route path="/home/user/:id" element={authenticated ? <DetailAd /> : <Navigate to="/home" />} />
+        <Route path="/admin/detailservice/:id" element={authenticated ? <DetailService /> : <Navigate to="/home" />}/>
+        <Route exact path="/admin" element={(authenticated && allow === 0) || (authenticated && allow === 2) ? (<Admin />) : (<Navigate to="/home" /> )}/> 
+        <Route path="/admin/customize" element={(authenticated && allow === 0)? (<Customize/>) : (<Navigate to="/home" /> )}/> 
+        <Route path="/admin/providers" element={(authenticated && allow === 0) ? (<Proveedores/>) : (<Navigate to="/home" /> )}/> 
+        <Route path="/admin/services" element={(authenticated && allow === 0) ? (<Services/>) : (<Navigate to="/home" /> )}/> 
+        <Route path="/admin/:name" element={(authenticated && allow === 0) || (authenticated && allow === 2) ? (<Admin /> ) : (<Navigate to="/error" /> )} /> 
+        <Route path="/admin/dett/:id" element={(authenticated && allow === 0) || (authenticated && allow === 2) ? (<DetailAd />) : (<Navigate to="/error" /> ) } /> 
+        <Route path={"*"} element={<Navigate to="/error" />} /> 
+        <Route path="/tips" element={<Tips />} /> 
+      </Routes> 
       <Footer />
       <Agencia />
     </div>
