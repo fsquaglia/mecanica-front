06ddec80d0f75415eb055563add0806c42589ef9
fsquaglia <fernandosquaglia@gmail.com>
2024-05-01 21:08:44 -0300

    reescribiendo Navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import style from "./Navbar.module.css";
-import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import { NavLink, Navigate, useLocation, useNavigate } from "react-router-dom";
 import { Link, animateScroll as scroll } from "react-scroll";
 import { useAuth } from "../Auth/AuthContext/AuthContext";
 import { AdminLink, LoginLinks, PanelAdmin } from "./Links/Index";
@@ -10,6 +10,7 @@ import userLogo from "../../assets/images/user.png";
 const Navbar = () => {
   const { authenticated, user, logout } = useAuth();
   const { pathname } = useLocation();
+  const navigate = useNavigate();
 
   const admin = user?.role === 0 ? true : false;
   const noRoutes =
@@ -24,124 +25,134 @@ const Navbar = () => {
     logout();
   };
 
+  const toLogin = () => {
+    if (!authenticated && pathname === "/") navigate("/login");
+  };
+
   return (
     <div className={style.nav}>
       <div className={style.logo}>
-        <img src={logo} alt="" className={style.logoImg} />
+        <NavLink to="/">
+          <img
+            src={logo}
+            alt="Logo Taller Boscarol"
+            className={style.logoImg}
+          />
+        </NavLink>
       </div>
 
       <div className={style.login}>
         {authenticated && location.pathname !== "/" ? (
           <>
             <NavLink to="/">
-              <h3 className={style.linksH3}>Home</h3>
+              <h3 className={style.linksH3}>Home</h3>{" "}
             </NavLink>
-            {admin && noRoutes ? <PanelAdmin /> : null}
+            {admin && noRoutes ? (
+              <>
+                <PanelAdmin />
+              </>
+            ) : null}
           </>
         ) : (
           <>
-            {location.pathname === "/" && (
-              <Link
-                to="/pagina"
-                activeClass="active"
-                smooth={true}
-                duration={600}
-                offset={-70}
-                activeStyle={{ color: "red" }}
-              >
-                <h3 className={style.linksH3}>Home</h3>
-              </Link>
-            )}
-            {location.pathname !== "/tips" && (
-              <>
-                <Link
-                  to="/historia"
-                  activeClass="active"
-                  smooth={true}
-                  duration={600}
-                  offset={-70}
-                  activeStyle={{ color: "red" }}
-                >
-                  <h3 className={style.linksH3}>Historia</h3>
-                </Link>
-                <Link
-                  to="/servicios"
-                  activeClass="active"
-                  smooth={true}
-                  duration={600}
-                  offset={-70}
-                  activeStyle={{ color: "red" }}
-                >
-                  <h3 className={style.linksH3}>Servicios</h3>
-                </Link>
-                <Link
-                  to="/consejos"
-                  activeClass="active"
-                  smooth={true}
-                  duration={600}
-                  offset={-70}
-                  activeStyle={{ color: "red" }}
-                >
-                  <h3 className={style.linksH3}>Tips</h3>
-                </Link>
-                <Link
-                  to="/contacto"
-                  activeClass="active"
-                  smooth={true}
-                  duration={600}
-                  offset={-70}
-                >
-                  <h3 className={style.linksH3}>Contacto</h3>
-                </Link>
-              </>
-            )}
+            {" "}
+            <NavLink
+              to="/"
+              activeClass="active"
+              smooth="true"
+              duration={600}
+              offset={-70}
+              activeStyle={{ color: "red" }}
+            >
+              <h3 className={style.linksH3}>Home</h3>{" "}
+            </NavLink>
+            <NavLink
+              to="historia"
+              activeClass="active"
+              smooth="true"
+              duration={600}
+              offset={-70}
+              activeStyle={{ color: "red" }}
+            >
+              {" "}
+              <h3 className={style.linksH3}>Historia</h3>{" "}
+            </NavLink>
+            <Link
+              to="servicios"
+              activeClass="active"
+              smooth="true"
+              duration={600}
+              offset={-70}
+              activeStyle={{ color: "red" }}
+            >
+              {" "}
+              <h3 className={style.linksH3}>Servicios</h3>{" "}
+            </Link>
+            <Link
+              to="consejos"
+              activeClass="active"
+              smooth="true"
+              duration={600}
+              offset={-70}
+              activeStyle={{ color: "red" }}
+            >
+              {" "}
+              <h3 className={style.linksH3}>Tips</h3>{" "}
+            </Link>
+            <Link
+              to="contacto"
+              activeClass="active"
+              smooth="true"
+              duration={600}
+              offset={-70}
+            >
+              {" "}
+              <h3 className={style.linksH3}>Contacto</h3>{" "}
+            </Link>
           </>
         )}
-
-        {/* {authenticated && location.pathname !== "/" ?
-          <>
-            <NavLink to='/' ><h3 className={style.linksH3}>Home</h3> </NavLink>
-            {admin && noRoutes ? <>
-              <PanelAdmin />
-            </> : null}
-          </> :
-
-          <> <Link to="pagina" activeClass="active" smooth={true} duration={600} offset={-70} activeStyle={{ color: 'red' }}><h3 className={style.linksH3}>Home</h3> </Link>
-            <Link to="historia" activeClass="active" smooth={true} duration={600} offset={-70} activeStyle={{ color: 'red' }}> <h3 className={style.linksH3}>Historia</h3> </Link>
-            <Link to="servicios" activeClass="active" smooth={true} duration={600} offset={-70} activeStyle={{ color: 'red' }}>   <h3 className={style.linksH3}>Servicios</h3> </Link>
-            <Link to="consejos" activeClass="active" smooth={true} duration={600} offset={-70} activeStyle={{ color: 'red' }}>   <h3 className={style.linksH3}>Tips</h3> </Link>
-            <Link to="contacto" activeClass="active" smooth={true} duration={600} offset={-70} >  <h3 className={style.linksH3}>Contacto</h3> </Link>
-          </>} */}
       </div>
 
       <div className={style.userData}>
-        <div class="dropdown">
+        {!authenticated && pathname === "/" ? (
           <button
-            class={`btn btn-secondary ${
-              authenticated ? style.dropDownButtonHidden : style.dropDownButton
-            }`}
+            className={`btn btn-secondary ${style.dropDownButton}`}
             type="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
+            onClick={toLogin}
           >
-            {authenticated ? (
-              <ShowUser />
-            ) : (
-              <img src={userLogo} className={style.userLogo} />
-            )}
+            <img src={userLogo} className={style.userLogo} />
           </button>
-          <ul class={`dropdown-menu ${style.dropdownMenu}`}>
-            <LoginLinks />
-            <AdminLink />
-            <li>
+        ) : (
+          <div className="dropdown">
+            <button
+              className={`btn btn-secondary ${
+                authenticated
+                  ? style.dropDownButtonHidden
+                  : style.dropDownButton
+              }`}
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
               {authenticated ? (
-                <a className={style.out} href="/" onClick={handleClick}>
-                  Salir
-                </a>
-              ) : null}
-            </li>
-          </ul>
-        </div>
+                <ShowUser />
+              ) : (
+                <img src={userLogo} className={style.userLogo} />
+              )}
+            </button>
+            <ul className={`dropdown-menu ${style.dropdownMenu}`}>
+              <LoginLinks />
+              <AdminLink />
+              <li>
+                {authenticated ? (
+                  <a className={style.out} href="/" onClick={handleClick}>
+                    Salir
+                  </a>
+                ) : null}
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
 
       <button
@@ -152,38 +163,35 @@ const Navbar = () => {
         aria-controls="offcanvasDarkNavbar"
         aria-label="Toggle navigation"
       >
-        <span class={style.togglerIcon}>&#9776;</span>
+        <span className={style.togglerIcon}>&#9776;</span>
       </button>
       <div
-        class="offcanvas offcanvas-end text-bg-dark"
-        tabindex="-1"
+        className="offcanvas offcanvas-end text-bg-dark"
+        tabIndex="-1"
         id="offcanvasDarkNavbar"
         aria-labelledby="offcanvasDarkNavbarLabel"
-        backdrop={false}
+        backdrop="false"
       >
-        <div class={`offcanvas-header ${style.divHeader}`}>
+        <div className={`offcanvas-header ${style.divHeader}`}>
           <button
             type="button"
-            class="btn-close btn-close-white"
+            className="btn-close btn-close-white"
             data-bs-dismiss="offcanvas"
             aria-label="Close"
           ></button>
           <h2
-            class={`offcanvas-title ${style.titleDeployMenu}`}
+            className={`offcanvas-title ${style.titleDeployMenu}`}
             id="offcanvasDarkNavbarLabel"
-          >
-            {" "}
-            Menú de Navegación
-          </h2>
+          ></h2>
         </div>
-        <div class="offcanvas-body">
+        <div className="offcanvas-body">
           <br></br>
 
-          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-            <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">
+          <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">
               {authenticated ? `Bienvenido ${user.nickname}` : null}
             </h5>
-            <li class={`nav-item ${style.divPic}`}>
+            <li className={`nav-item ${style.divPic}`}>
               {authenticated && (
                 <img
                   className={style.pictureDeployMenu}
@@ -192,30 +200,25 @@ const Navbar = () => {
                 />
               )}
             </li>
-            {authenticated ? (
+            {/* {authenticated ? null : (
               <>
-                <br></br>
-                <br></br>
-              </>
-            ) : (
-              <>
-                <li class="nav-item" data-bs-dismiss="offcanvas">
+                <li className="nav-item" data-bs-dismiss="offcanvas">
                   <NavLink to="/login">
                     <a className={style.out} aria-current="page" href="#">
-                      Ingresar
+                      Ingreso
                     </a>
                   </NavLink>
                 </li>
               </>
-            )}
-            <li class="nav-item" data-bs-dismiss="offcanvas">
+            )} */}
+            <li className="nav-item" data-bs-dismiss="offcanvas">
               <LoginLinks />
             </li>
-            <li class="nav-item" data-bs-dismiss="offcanvas">
+            <li className="nav-item" data-bs-dismiss="offcanvas">
               <AdminLink />
             </li>
             <br></br>
-            <li class="nav-item" data-bs-dismiss="offcanvas">
+            <li className="nav-item" data-bs-dismiss="offcanvas">
               {authenticated ? (
                 <a className={style.out} href="/" onClick={handleClick}>
                   Salir
