984ab9ffb24361841b2316a1049bef33ff391bfc
Kevin Osmar Pauluk <osmarkevinp@gmail.com>
2024-05-05 16:38:07 -0300

    search de tips funcionando

--- client/src/components/cards/Cards.jsx
+++ client/src/components/cards/Cards.jsx
@@ -6,12 +6,15 @@ const Cards = ({ info, elModal }) => {
   return (
     <div className={style.bigDiv}>
       {
-        info && info.map((el) => {
-          const { titlePost, imgPost, textPost } = el
-          return (
-            <Card titulo={titlePost} imagen={imgPost} descripcion={textPost} elModal={elModal} />
-          )
-        })
+        info.length > 0 ?
+          (info && info.map((el) => {
+            const { titlePost, imgPost, textPost } = el
+            return (
+              <Card titulo={titlePost} imagen={imgPost} descripcion={textPost} elModal={elModal} />
+            )
+          }))
+          :
+          <h2 className={style.h2Error}>¡Vaya! no hemos encontrado ese tip.</h2>
       }
     </div>
   )
